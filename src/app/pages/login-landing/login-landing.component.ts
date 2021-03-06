e6d813e997530462b2ee31a0a476d0c68f82e82c
Xavier Martinez <blackshieldsx@gmail.com>
2021-03-05 21:26:34 -0800

    Created the new anding page

---
+++ src/app/pages/login-landing/login-landing.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms';
+import { emailValidator } from '../../@theme/utils/app-validators';
+import { AppSettings } from '../../app.settings';
+import { Settings } from '../../app.settings.model';
+import { ApiService } from '../../@core/services/api.service';
+import { CookieService } from 'ngx-cookie-service';
+
+interface TokenObj {
+  token: string;
+}
+@Component({
+  selector: 'app-login',
+  templateUrl: './login-landing.component.html'
+})
+export class LoginLandingComponent implements OnInit {
+  public form: FormGroup;
+  public settings: Settings;
+  authForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+  registerMode = false;
+  constructor(
+    private apiService: ApiService,
+    private cookieService: CookieService,
+    private router: Router,
+    public appSettings: AppSettings,
+    public fb: FormBuilder
+  ){
+    this.settings = this.appSettings.settings;
+    this.form = this.fb.group({
+      username: [null, Validators.compose([Validators.required, Validators.minLength(4)])],
+      password: [null, Validators.compose([Validators.required, Validators.minLength(6)])],
+      rememberMe: false
+    });
+  }
+  ngOnInit() {
+    const crToken = this.cookieService.get('cr-token');
+    // if (crToken) {
+    //   this.router.navigate(['/pages/xcards']);
+    // }
+  }
+
+  loginUsers() {
+    this.apiService.loginUser(this.authForm.value).subscribe(
+      (result: TokenObj) => {
+        this.cookieService.set('cr-token', result.token);
+        this.router.navigate(['/pages/xcards']);
+      },
+        error => console.log(error)
+    );
+  }
+
+  // tslint:disable-next-line:ban-types
+  public onSubmit(values: Object): void {
+    if (this.form.valid) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  // tslint:disable-next-line:typedef use-lifecycle-interface
+  ngAfterViewInit(){
+    setTimeout(() => {
+      this.settings.loadingSpinner = false;
+    });
+  }
+}
---
+++ src/app/pages/login-landing/login-landing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../../@core/shared/shared.module';
+import { CookieService } from 'ngx-cookie-service';
+
+import { LoginLandingComponent } from './login-landing.component';
+
+export const routes = [
+  { path: '', component: LoginLandingComponent, pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule
+  ],
+  declarations: [
+    LoginLandingComponent
+  ],
+  providers: [
+    CookieService
+  ]
+})
+export class LoginLandingModule { }
